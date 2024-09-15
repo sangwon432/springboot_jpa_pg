bdd585141a763ae3a947ba0aaf1d08d19fa22787
sangwon432 <swonl0622@gmail.com>
2024-09-15 04:59:01 -0700

    connect DB with env config

---
+++ src/main/java/com/sangwon/springboot_jpa_pg/SpringbootJpaPgApplication.java
@@ -0,0 +1,13 @@
+package com.sangwon.springboot_jpa_pg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootJpaPgApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootJpaPgApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sangwon/springboot_jpa_pg/SpringbootJpaPgApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sangwon.springboot_jpa_pg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootJpaPgApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
