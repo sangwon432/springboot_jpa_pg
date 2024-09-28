61b4859a2cf00aa5f67b7f4d049bfa5a587623a7
sangwon432 <swonl0622@gmail.com>
2024-09-28 07:43:50 -0700

    add User Entity

---
+++ src/main/java/com/sangwon/springboot_jpa_pg/entities/User.java
@@ -0,0 +1,28 @@
+package com.sangwon.springboot_jpa_pg.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    @Column(nullable = false, unique = true)
+    private String username;
+    @Column(nullable = false, unique = true)
+    private String email;
+    @Column(nullable = false)
+    private String password;
+
+
+}
